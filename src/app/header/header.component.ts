37dcf93690835082df9ad54d34318cd899fde049
Buzz12345 <bluecatsashakhademscky@gmail.com>
2022-04-20 19:22:37 +0300

    Add mobile styles for [add-hero, dashboard, header, hero-search, message]. Change style for [messages]

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,6 +10,23 @@ export class HeaderComponent implements OnInit {
 
   public title: string = 'Tour of Heroes';
 
+  public toggleSearch(): void {
+    let searchBtn = document.getElementById('searchBtn');
+    let search = document.getElementById('headerSearch');
+    let navbar = document.getElementById('headerNav');
+
+    if (searchBtn?.classList.contains('active')) {
+      searchBtn?.classList.remove('active');
+      search?.classList.remove('active');
+      navbar?.classList.remove('active');
+    }
+    else {
+      searchBtn?.classList.add('active');
+      search?.classList.add('active');
+      navbar?.classList.add('active');
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
